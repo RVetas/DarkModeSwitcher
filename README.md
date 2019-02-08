4fcbc15b6c1d1ee1d141b18b3e5ddc8180756556
Vetas <ram010172@yandex.ru>
2019-02-08 12:13:35 +0300

    Some scheduling changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # DarkModeSwitcher
-*version 0.1b* -- this is early beta version and parameters of the daemon will be changed
+*version 0.2b* -- this is early beta version and parameters of the daemon will be changed
 
 This package provides you an opportunity to schedule Dark Mode switching on your Mac.
--- Sources/DarkModeSwitcher/main.swift
+++ Sources/DarkModeSwitcher/main.swift
@@ -12,7 +12,7 @@ let userCalendar = Calendar.current
 let currentDate = Date()
 let currentHour = userCalendar.component(.hour, from: currentDate)
 
-if currentHour > 20 || currentHour < 10 {
+if currentHour >= 20 || currentHour =< 10 {
     let command = "tell application \"System Events\" to tell appearance preferences to set dark mode to true"
     let _ = NSAppleScript(source: command)?.executeAndReturnError(nil).stringValue
     print("Ok, its >20 or < 10")
